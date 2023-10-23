232a598554bc2a897a453aaca1b77db465511a2a
Eric Gauzens <ericgauzens@hotmail.com>
2023-10-23 19:47:28 -0400

    Added styling for buttons, messages, notifications

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
   </header>
 
   <main>
+
     <div class="tooltip">
       <sparc-tooltip v-for="dir in tooltipDirs" :key="dir" :placement="dir">
         <template v-slot:data>
@@ -24,6 +25,28 @@
       <template v-slot:data><div>Only show this tooltip if content is cutoff</div></template>
       <template v-slot:item><div class="tooltip-item">Only show tooltip when this has ellipsis</div></template>
     </sparc-tooltip>
+    <el-button class="primary">Primary Button</el-button>
+    <el-button class="secondary">Secondary Button</el-button>
+    <el-button
+      @click="openSuccessMessage">
+      Show Success Notification
+    </el-button>
+    <el-button
+      @click="openInfoMessage">
+      Show Info Notification
+    </el-button>
+    <el-button
+      @click="openFailMessage">
+      Show Failure Notification
+    </el-button>
+    <el-button
+      @click="openNotification">
+      Show Notification
+    </el-button>
+    <el-button
+      @click="openNotificationWithIcon">
+      Show Notification with Icon
+    </el-button>
   </main>
 </template>
 
@@ -31,6 +54,9 @@
   import HelloWorld from './components/HelloWorld.vue'
   import SparcTooltip from './components/SparcTooltip.vue'
   import { ref } from 'vue'
+
+  import { successMessage, infoMessage, failMessage, informationNotification, iconInformationNotification } from "../utils/notificationMessages"
+
   export default {
     components: {
       HelloWorld,
@@ -56,11 +82,28 @@
       return {
         tooltipDirs
       }
+    },
+    methods: {
+      openSuccessMessage() {
+        successMessage(`Success!`)
+      },
+      openFailMessage() {
+        failMessage(`Failure.`)
+      },
+      openInfoMessage() {
+        infoMessage(`Information`)
+      },
+      openNotification() {
+        informationNotification('Notification Title', 'This is a notification.')
+      },
+      openNotificationWithIcon() {
+        iconInformationNotification('Notification Title', 'This is a notification with an icon.')
+      }
     }
   }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 header {
   line-height: 1.5;
 }
