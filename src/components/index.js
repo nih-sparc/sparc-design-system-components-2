b88b2e74e48d35395b4f5c3f557fa8ec9821b2da
Eric Gauzens <ericgauzens@hotmail.com>
2023-11-02 18:20:02 -0400

    updated exported plugin

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,23 @@
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
 import '../assets/styles.scss';
 
-import SparcLogo from './SparcLogo.vue'
+const installYourPlugin = (app) => {
+  app.use(ElementPlus);
+  // Register and expose your custom components and logic here
+  // For example:
+  // app.component('CustomComponent', CustomComponent);
+  Object.keys(Components).forEach(component => {
+    app.component(component, Components[component]);
+  })
+};
 
-const Components = {
-  SparcLogo
-}
-
-import * as ElementPlus from 'element-plus';
-
-export default { ElementPlus, ...Components };
+export default {
+  install(app) {
+    const vue3App = createApp(app);
+    installYourPlugin(vue3App);
+    // Add any custom components or logic here
+    // For example:
+    // vue3App.component('CustomComponent', CustomComponent);
+  },
+};
