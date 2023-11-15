7e0f40cc78682239729454c5266cebaec0552a00
Eric Gauzens <ericgauzens@hotmail.com>
2023-11-15 16:44:19 -0500

    add id injection key for element plus

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,15 @@
 import ElementPlus from 'element-plus';
+import { ID_INJECTION_KEY } from 'element-plus'
 import '../assets/styles.scss';
 
 import SparcLogo from './SparcLogo.vue';
 
 export default {
   install(app) {
+    app.provide(ID_INJECTION_KEY, {
+      prefix: 1024,
+      current: 0,
+    });
     app.use(ElementPlus);
     app.component('SparcLogo', SparcLogo);
   },
