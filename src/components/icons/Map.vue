e0301b0280d8c35792ea1c34a28bb4684003bc39
Samantha Kraft <samantha@brb-02197.apn.wlan.private.upenn.edu>
2023-12-05 10:29:49 -0500

    added icon components that I missed

---
+++ src/components/icons/Map.vue
@@ -0,0 +1,32 @@
+<template>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      :width="width"
+      :height="height"
+      :fill="color"
+      viewBox="0 0 13 17"
+    >
+      <g _fill="#000" fill-rule="evenodd"><path pid="0" d="M6.478 0C10.072 0 13 2.95 13 6.527c0 4.203-2.485 7.422-5.546 10.104a1.418 1.418 0 01-1.908 0C2.485 13.95 0 10.73 0 6.527 0 2.951 2.928 0 6.478 0zM6.5 4A2.51 2.51 0 019 6.5C9 7.893 7.893 9 6.5 9A2.51 2.51 0 014 6.5C4 5.148 5.148 4 6.5 4z"/></g>
+    </svg>
+  </template>
+  
+  <script>
+  export default {
+    name: "MapIcon",
+    props: {
+      width: {
+        type: Number,
+        default: 13,
+      },
+      height: {
+        type: Number,
+        default: 17,
+      },
+      color: {
+        type: String,
+        default: "currentColor",
+      },
+    },
+  };
+  </script>
+
